ef2cc0ab0ad03b4ae52a910a1994dfb61e8e0e70
Martin Vavro <vavroma6@fel.cvut.cz>
2020-08-16 00:23:37 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int countWords( FILE *file)
+{
+  char c;
+  int count = 0;
+  while ((c = fgetc(file)) != EOF)
+  {
+    if (c == ' ') count++;
+  }
+
+  return count;
+}
+
+char* getWord(FILE *file, int number)
+{
+
+  rewind(file);
+  int count = 0;
+  char *word = malloc (sizeof(char) * 50);
+
+  while (fscanf(file, " %49s", word) != EOF)
+  {
+    if (++count == number)
+    {
+      strcat(word, " ");
+      return word;
+    }
+  }
+
+  return "";
+}
+
+int getBlessingSize(int size)
+{
+  int total = 0;
+  int current;
+
+  for (int i = 0; i < 100; i++)
+  {
+    total += rand() % (30 + size);
+  }
+
+  return total/100;
+}
+
+int main (int argc, char *argv [])
+{
+  srand(time(NULL));
+  FILE *sacredText;
+  char wordsOfGod[1000] = {0};
+
+  sacredText = fopen("god.txt", "r");
+  int numberOfWordsInFile = countWords(sacredText);
+
+  int offering = (rand() % 20) - 10;
+
+  for (int i = 0; i < getBlessingSize(offering); i++)
+  {
+    strcat(wordsOfGod, getWord(sacredText, rand() % numberOfWordsInFile));
+  }
+
+  fclose(sacredText);
+
+  printf("GOD SAYS:\n %s\n", wordsOfGod);
+
+}
